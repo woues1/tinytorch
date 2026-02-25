04a24f4f37e079ac15c3b742d41b7ac938ce92e0
woues1 <toni.hirvikallio@gmail.com>
2026-02-25 21:05:43 +0200

    feat: add element-wise exponential and max along dimension functions to Tensor

--- src/tensor/reductions.rs
+++ src/tensor/reductions.rs
@@ -20,4 +20,46 @@ where
         let number_of_items = FromPrimitive::from_usize(count).unwrap();
         self.sum_all() / number_of_items
     }
+
+    /// Element-wise exponential (e^x)
+    pub fn exp(&self) -> Self {
+        let new_data = self.data.iter().map(|x| x.exp()).collect();
+        Tensor::new(new_data, self.shape.clone()).unwrap()
+    }
+
+    pub fn max_dim(&self, dim: usize) -> Self {
+        let mut new_shape = self.shape.clone();
+        new_shape[dim] = 1;
+
+        let mut new_strides = vec![0; new_shape.len()];
+        let mut current_stride = 1;
+        for i in (0..new_shape.len()).rev() {
+            new_strides[i] = current_stride;
+            current_stride *= new_shape[i];
+        }
+
+        let total_elements = new_shape.iter().product();
+        let mut new_data = vec![T::min_value(); total_elements];
+
+        for i in 0..self.data.len() {
+            let mut current_val = i;
+            let mut target_idx = 0;
+
+            for d in (0..self.shape.len()).rev() {
+                let coord = current_val % self.shape[d];
+                current_val /= self.shape[d];
+                if d != dim {
+                    target_idx += coord * new_strides[d];
+                }
+            }
+
+            new_data[target_idx] = new_data[target_idx].max(self.data[i]);
+        }
+
+        Tensor {
+            data: new_data,
+            shape: new_shape,
+            strides: new_strides,
+        }
+    }
 }
